17ff2123822e21be5162fe671f77fc19af1b16be
ManjunathJ-MMSG <ManjunathJ@mmsg.com.au>
2024-07-11 09:19:14 +1000

    Updating model and styles for the savings card block

--- blocks/savingscard/savingscard.js
+++ blocks/savingscard/savingscard.js
@@ -1,8 +1,9 @@
 export default function decorate(block) {
+  debugger;
   const [titleBg, title, sectionDesc, sectionNoOly, sectionWithOly] = block.children;
   titleBg.className = 'bg_image';
   title.className = 'fg_overlay_content';
   sectionDesc.className = 'section_desc';
-  sectionNoOly.className = 'section_no_oly';
-  sectionWithOly.className = 'section_with_oly';
+  sectionNoOly.className = 'no_oly';
+  sectionWithOly.className = 'with_oly';
 }
