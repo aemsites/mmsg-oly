423fdcb15c6412d36d09f4a06d01f84d8a892aba
bhupesh-99 <pipley@adobe.com>
2024-07-10 12:11:17 +0530

    code formatted using prettier

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,5 +1,5 @@
-import { getMetadata } from "../../scripts/aem.js";
-import { loadFragment } from "../fragment/fragment.js";
+import { getMetadata } from '../../scripts/aem.js';
+import { loadFragment } from '../fragment/fragment.js';
 
 /**
  * loads and decorates the footer
@@ -7,30 +7,28 @@ import { loadFragment } from "../fragment/fragment.js";
  */
 export default async function decorate(block) {
   // load footer as fragment
-  const footerMeta = getMetadata("footer");
-  const footerPath = footerMeta
-    ? new URL(footerMeta, window.location).pathname
-    : "/footer";
+  const footerMeta = getMetadata('footer');
+  const footerPath = footerMeta ? new URL(footerMeta, window.location).pathname : '/footer';
   const fragment = await loadFragment(footerPath);
 
   // decorate footer DOM
-  block.textContent = "";
-  const footer = document.createElement("div");
+  block.textContent = '';
+  const footer = document.createElement('div');
   while (fragment.firstElementChild) footer.append(fragment.firstElementChild);
 
-  const footerNavDiv = document.createElement("div");
-  footerNavDiv.className = "footer-nav-div";
-  const footerNav = footer.querySelector(".footer-nav");
-  const footerPElements = footerNav.querySelectorAll(":scope div > p");
+  const footerNavDiv = document.createElement('div');
+  footerNavDiv.className = 'footer-nav-div';
+  const footerNav = footer.querySelector('.footer-nav');
+  const footerPElements = footerNav.querySelectorAll(':scope div > p');
   footerNavDiv.append(...footerPElements);
 
-  const nav = document.createElement("nav");
-  nav.className = "footer-nav-links";
-  const links = footer.querySelector(".footer-nav ul");
+  const nav = document.createElement('nav');
+  nav.className = 'footer-nav-links';
+  const links = footer.querySelector('.footer-nav ul');
   nav.append(links);
   footerNavDiv.append(nav);
 
-  footerNav.innerHTML = "";
+  footerNav.innerHTML = '';
   footerNav.append(footerNavDiv);
   block.append(footer);
 }
--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,15 +1,15 @@
 export default function decorate(block) {
   [...block.children].forEach((row) => {
-    if (row.querySelector("picture")) {
-      row.classList.add("hero-image");
+    if (row.querySelector('picture')) {
+      row.classList.add('hero-image');
     } else if (row.lastElementChild.children.length >= 1) {
-      row.classList.add("hero-content");
+      row.classList.add('hero-content');
     } else {
-      row.classList.add("hero-inactive");
+      row.classList.add('hero-inactive');
     }
   });
-  const div = document.createElement("div");
-  div.classList.add("content");
+  const div = document.createElement('div');
+  div.classList.add('content');
   div.append(...block.children);
   block.append(div);
 }
