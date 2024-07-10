0ead1af210f9824867c686c8d4e8b08d1970d48e
bhupesh-99 <pipley@adobe.com>
2024-07-10 11:16:42 +0530

    merged with main branch

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,6 @@
 module.exports = {
   root: true,
-  extends: [
-    'airbnb-base',
-    'plugin:json/recommended',
-    'plugin:xwalk/recommended',
-  ],
+  extends: ['airbnb-base', 'plugin:json/recommended', 'plugin:xwalk/recommended', 'prettier'],
   env: {
     browser: true,
   },
@@ -18,5 +14,6 @@ module.exports = {
     'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
+    'import/prefer-default-export': 'error',
   },
 };
