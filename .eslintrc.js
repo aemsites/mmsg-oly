1c72753b6e247ec27341228b4368bfebd0c7ce32
roohi.bengani <roohibengani@gmail.com>
2024-07-11 10:59:54 +0530

    footer changes wrt figma

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   rules: {
     'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
-    'linebreak-style': ['error', 'windows'], // enforce unix linebreaks
+    'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
     'import/prefer-default-export': 'error',
   },
