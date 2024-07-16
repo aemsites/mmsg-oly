67a93656cce03de917d512fbadac84969709a7b0
roohi.bengani <roohibengani@gmail.com>
2024-07-16 13:47:32 +0530

    chnages for car-card component

--- blocks/car-card/car-card.js
+++ blocks/car-card/car-card.js
@@ -4,13 +4,13 @@ import myJsonObject from '../../scripts/data.js';
 function getFeaturesContainer() {
   return `
       <div class="car-features-container">
-        <div class="car-feature">
+        <div class="car-feature camera">
           <span>360 Camera</span>
         </div>
-        <div class="car-feature">
+        <div class="car-feature alloy-wheels">
           <span>Alloy wheels</span>
         </div>
-        <div class="car-feature">
+        <div class="car-feature seats">
           <span>Heated seats</span>
         </div>
       </div>
@@ -20,9 +20,10 @@ function getFeaturesContainer() {
 function getPriceContainer(cardObj) {
   return `
       <div class="car-card-price-container">
-        <p class="car-card-price-title">Tax Saving</p>
-        <p class="car-card-price">${cardObj?.price}</p>
-        <p class="car-card-price-weekly">Weekly Price Starts From ${cardObj.weeklyPriceInfo}</p>
+        <p class="car-card-price-title">Weekly price from</p>
+        <h1 class="car-card-price">${cardObj?.weeklyPriceInfo}</h1>
+        <p>Including all car running costs</p>
+        <p class="car-card-price-weekly">Estimated tax savings ${cardObj.price}</p>
       <div>
     `;
 }
@@ -32,9 +33,6 @@ function getButtonContainer() {
     <div class="car-card-button-container">
       <p class="button-container">
         <a href="oly.com.au" title="View Offer Details" class="button">View Offer Details</a>
-      </p>
-      <p class="button-container">
-        <a href="oly.com.au" title="Request A Call" class="button primary">Request A Call</a>
       </p>
     </div>
     `;
@@ -47,7 +45,7 @@ function buildCardTemplate(cardObj) {
 
   const basicCarDetailContainer = `
       <div class="container">
-        <h2 >${cardObj?.year} ${cardObj?.modelName}</h2>
+        <h3>${cardObj?.year} ${cardObj?.modelName}</h3>
         <img class="car-card-image" src="${imageLink}" alt="BMW 4 Series">
         <p class="car-card-description">${cardObj?.carDescription || ''}</p>
       <div>
@@ -68,12 +66,10 @@ function buildCards(carData) {
   const wrapper = document.createElement('div');
   wrapper.className = 'car-cards-wrapper';
   wrapper.innerHTML = carData.map(buildCardTemplate).join('');
-  // appendCardListeners(wrapper);
   return wrapper;
 }
 
 export default function decorate(block) {
   const result = buildCards(myJsonObject);
   block.append(result);
-  // return result;
 }
