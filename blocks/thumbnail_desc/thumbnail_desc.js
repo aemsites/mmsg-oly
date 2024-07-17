ae221e6297cd5a9d3ec0a71dcd8d17f90881b4af
VJ <vishnu.vijay@mmsg.com.au>
2024-07-17 15:21:03 +1000

    hot fixes

--- blocks/thumbnail_desc/thumbnail_desc.js
+++ blocks/thumbnail_desc/thumbnail_desc.js
@@ -1,1 +1,7 @@
-export default function decorate(block) {}
+export default function decorate(block) {
+  [...block.children].forEach((row) => {});
+  const div = document.createElement('div');
+  div.classList.add('content');
+  div.append(...block.children);
+  block.append(div);
+}
