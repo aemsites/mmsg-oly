4b619b34d329b6da82ea1934214852ab968ca1ef
VJ <vishnu.vijay@mmsg.com.au>
2024-07-17 15:22:47 +1000

    hotfix

--- blocks/thumbnail_desc/thumbnail_desc.js
+++ blocks/thumbnail_desc/thumbnail_desc.js
@@ -1,5 +1,13 @@
 export default function decorate(block) {
-  [...block.children].forEach((row) => {});
+  [...block.children].forEach((row) => {
+    if (row.querySelector('picture')) {
+      row.classList.add('hero-image');
+    } else if (row.lastElementChild.children.length >= 1) {
+      row.classList.add('hero-content');
+    } else {
+      row.classList.add('hero-inactive');
+    }
+  });
   const div = document.createElement('div');
   div.classList.add('content');
   div.append(...block.children);
