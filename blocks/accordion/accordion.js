b952c2ca0e298630b5b4af200f7c1f7cae6928c2
Mrugesh Panchal <mpanchal@adobe.com>
2024-07-18 17:43:28 +0530

    accordion base component and css updated

--- blocks/accordion/accordion.js
+++ blocks/accordion/accordion.js
@@ -1,33 +1,21 @@
-/*
- * Accordion Block
- * Recreate an accordion
- * https://www.hlx.live/developer/block-collection/accordion
- */
-
-function hasWrapper(el) {
-  return !!el.firstElementChild && window.getComputedStyle(el.firstElementChild).display === 'block';
+/* this function also gets called by accordion-group */
+export function generateAccordionDOM(block) {
+  const details = document.createElement('details');
+  const summary = document.createElement('summary');
+  details.append(summary);
+  Array.from(block.children).forEach((element, i) => {
+    if (i === 0) {
+      const heading = element.querySelector('h2,h3,h4,h5,h6');
+      summary.append(heading || element.textContent.trim());
+    } else {
+      details.append(element);
+    }
+  });
+  return details;
 }
 
 export default function decorate(block) {
-  [...block.children].forEach((row) => {
-    // decorate accordion item label
-    const label = row.children[0];
-    const summary = document.createElement('summary');
-    summary.className = 'accordion-item-label';
-    summary.append(...label.childNodes);
-    if (!hasWrapper(summary)) {
-      summary.innerHTML = `<p>${summary.innerHTML}</p>`;
-    }
-    // decorate accordion item body
-    const body = row.children[1];
-    body.className = 'accordion-item-body';
-    if (!hasWrapper(body)) {
-      body.innerHTML = `<p>${body.innerHTML}</p>`;
-    }
-    // decorate accordion item
-    const details = document.createElement('details');
-    details.className = 'accordion-item';
-    details.append(summary, body);
-    row.replaceWith(details);
-  });
+  const dom = generateAccordionDOM(block);
+  block.textContent = '';
+  block.append(dom);
 }
