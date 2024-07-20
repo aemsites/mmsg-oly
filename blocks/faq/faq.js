d39dfcc0982d7ad69363d2c978f16e5a31b6397d
Mrugesh Panchal <mpanchal@adobe.com>
2024-07-21 00:33:23 +0530

    smooth scroll added

--- blocks/faq/faq.js
+++ blocks/faq/faq.js
@@ -21,11 +21,26 @@ export default async function decorate(block) {
 
   // Check if form should be shown
   if (element && element.textContent.trim() === 'yes') {
+    const anchors = block.querySelectorAll('a[href^="#"]');
     block.classList.add('form');
     const form = await createForm(`${CONFIG.baseURL}/faqform.json`);
     element.replaceChildren(form);
     const searchInput = block.querySelector('#form-faqsearch');
     searchInput.addEventListener('input', handleSearchInput);
+
+    // Loop through each anchor and add a click event listener
+    anchors.forEach((anchor) => {
+      anchor.addEventListener('click', (event) => {
+        event.preventDefault(); // Prevent the default anchor behavior
+        // Get the target element id from the href attribute
+        const targetId = anchor.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+        // Scroll to the target element if it exists
+        if (targetElement) {
+          targetElement.scrollIntoView({ behavior: 'smooth' });
+        }
+      });
+    });
   } else {
     element.remove();
   }
