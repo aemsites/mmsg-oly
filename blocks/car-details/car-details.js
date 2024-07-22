f45192ffeeedb91e06d4901316de8029060b0c76
roohi.bengani <roohibengani@gmail.com>
2024-07-22 13:35:36 +0530

    Changes for car details component for Single card details page

---
+++ blocks/car-details/car-details.js
@@ -0,0 +1,31 @@
+const ISEV = ['yes', 'no'];
+
+export default function decorate(block) {
+  const cols = [...block.firstElementChild.children];
+  block.classList.add(`columns-${cols.length}-cols`);
+  const wrapper = document.createElement('div');
+  wrapper.className = 'car-detail-wrapper';
+
+  [...block.children].forEach((row) => {
+    [...row.children].forEach((col) => {
+      wrapper.append(col);
+      const firstChildDiv = col.children[0];
+      const isEV = firstChildDiv.textContent.trim();
+      if (ISEV.includes(isEV)) {
+        if (isEV === 'yes') {
+          col.classList.add('car-details-ev');
+        }
+        firstChildDiv.remove();
+      }
+      const pic = col.querySelector('picture');
+      if (pic) {
+        const picWrapper = pic.closest('div');
+        if (picWrapper && picWrapper.children.length === 1) {
+          picWrapper.classList.add('columns-img-col');
+        }
+      }
+    });
+  });
+  block.textContent = '';
+  block.append(wrapper);
+}
