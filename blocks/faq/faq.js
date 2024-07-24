f5cf475c7ea4c92f060782ec881db123ba20e215
Mrugesh Panchal <mpanchal@adobe.com>
2024-07-24 13:18:51 +0530

    FAQ minor search count fixes

--- blocks/faq/faq.js
+++ blocks/faq/faq.js
@@ -1,65 +1,68 @@
 import { createForm } from '../form/form.js';
 import { CONFIG } from '../../scripts/utils.js';
 
-function resetSearchInput(searchIcon, closeIcon) {
+function toggleBlocks(selector, isShow) {
+  const blocks = document.querySelectorAll(selector);
+  if (blocks && blocks.length > 0) {
+    if (isShow) {
+      blocks.forEach((block) => {
+        block.removeAttribute('style');
+      });
+    } else {
+      blocks.forEach((block) => {
+        block.style.display = 'none';
+      });
+    }
+  }
+}
+
+function resetSearchInput(searchIcon, closeIcon, searchMessage) {
   searchIcon.style.display = 'block';
   closeIcon.style.display = 'none';
+  searchMessage.style.display = 'none';
 
-  const tagsBlocks = document.querySelectorAll('.faq.block > div:nth-child(2)');
-  tagsBlocks.forEach((block) => {
-    block.removeAttribute('style');
-  });
+  toggleBlocks('.faq.block > div:nth-child(2)', true);
 
   const accordionGroupBlocks = document.querySelectorAll('.faq.accordion-group-container > .accordion-group-wrapper');
   accordionGroupBlocks.forEach((block) => {
-    // block.style.width = '504px';
     block.classList.remove('expanded');
   });
 
-  const defaultBlocks = document.querySelectorAll('.faq.accordion-group-container > .default-content-wrapper');
-  defaultBlocks.forEach((block) => {
-    block.removeAttribute('style');
-  });
-
-  const accordionBlocks = document.querySelectorAll('.accordion.block');
-  accordionBlocks.forEach((block) => {
-    block.removeAttribute('style');
-  });
+  toggleBlocks('.faq.accordion-group-container > .default-content-wrapper', true);
+  toggleBlocks('.accordion.block', true);
 }
 
-function handleSearchInput(searchIcon, closeIcon) {
+function handleSearchInput(searchIcon, closeIcon, searchMessage) {
   const searchTerm = document.querySelector('#form-faqsearch').value.toLowerCase();
   if (searchTerm.length > 0) {
     searchIcon.style.display = 'none';
     closeIcon.style.display = 'block';
 
-    const tagsBlocks = document.querySelectorAll('.faq.block > div:nth-child(2)');
-    tagsBlocks.forEach((block) => {
-      block.style.display = 'none';
-    });
+    toggleBlocks('.faq.block > div:nth-child(2)', false);
 
     const accordionGroupBlocks = document.querySelectorAll('.faq.accordion-group-container > .accordion-group-wrapper');
     accordionGroupBlocks.forEach((block) => {
-      // block.style.width = '100%';
       block.classList.add('expanded');
     });
 
     const accordionBlocks = document.querySelectorAll('.accordion.block');
+    let resultCount = 0;
     accordionBlocks.forEach((block) => {
       const blockContent = block.textContent.toLowerCase();
       if (blockContent.includes(searchTerm)) {
         block.style.display = 'block';
+        resultCount += 1;
       } else {
         block.style.display = 'none';
       }
     });
 
-    const defaultBlocks = document.querySelectorAll('.faq.accordion-group-container > .default-content-wrapper');
-    defaultBlocks.forEach((block) => {
-      block.style.display = 'none';
-    });
+    searchMessage.textContent = `You searched "${searchTerm}" (${resultCount} result${resultCount !== 1 ? 's' : ''} found)`;
+    searchMessage.style.display = 'block';
+
+    toggleBlocks('.faq.accordion-group-container > .default-content-wrapper', false);
   } else {
-    resetSearchInput(searchIcon, closeIcon);
+    resetSearchInput(searchIcon, closeIcon, searchMessage);
   }
 }
 
@@ -94,17 +97,23 @@ export default async function decorate(block) {
     closeText.classList.add('close-text');
     closeText.textContent = 'Close and back to FAQs';
 
-    // Append icons to the wrapper
+    // Create search message element
+    const searchMessage = document.createElement('div');
+    searchMessage.classList.add('search-message');
+    searchMessage.style.display = 'none';
+
+    // Append icons and search message to the wrapper
     const wrapper = document.querySelector('.field-wrapper');
     wrapper.appendChild(searchIcon);
     const closeContainer = document.createElement('div');
     closeContainer.classList.add('icon-text-container');
-    closeContainer.appendChild(closeIcon);
     closeContainer.appendChild(closeText);
+    closeContainer.appendChild(closeIcon);
     wrapper.appendChild(closeContainer);
+    wrapper.appendChild(searchMessage);
 
     searchInput.addEventListener('input', () => {
-      handleSearchInput(searchIcon, closeContainer);
+      handleSearchInput(searchIcon, closeContainer, searchMessage);
     });
 
     // Loop through each anchor and add a click event listener
@@ -130,9 +139,9 @@ export default async function decorate(block) {
       searchInput.value = '';
       searchIcon.style.display = 'block';
       closeContainer.style.display = 'none';
+      searchMessage.style.display = 'none';
       searchInput.focus();
-      // handleSearchInput(searchIcon, closeContainer);
-      resetSearchInput(searchIcon, closeContainer);
+      resetSearchInput(searchIcon, closeContainer, searchMessage);
     });
 
     // Clear input when close icon is activated via keyboard
@@ -142,9 +151,9 @@ export default async function decorate(block) {
         searchInput.value = '';
         searchIcon.style.display = 'block';
         closeContainer.style.display = 'none';
+        searchMessage.style.display = 'none';
         searchInput.focus();
-        // handleSearchInput(searchIcon, closeContainer);
-        resetSearchInput(searchIcon, closeContainer);
+        resetSearchInput(searchIcon, closeContainer, searchMessage);
       }
     });
   } else {
