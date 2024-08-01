c9463d69892de34acd74892c2619d43f97ab29d5
mojojain <manosezen@gmail.com>
2024-08-01 10:32:57 +0530

    added ev option

--- blocks/thumbnail-car/thumbnail-car.js
+++ blocks/thumbnail-car/thumbnail-car.js
@@ -2,5 +2,7 @@ import { createEVIcon } from '../../scripts/utils.js';
 
 export default function decorate(block) {
   block.children[1].append(block.children[2]);
-  createEVIcon(block.children[3].children[0], block.children[3]);
+  createEVIcon(block.children[2].children[0], block.children[2]);
+  block.children[2].classList.add('ev-badge');
+  block.children[1].append(block.children[2]);
 }
