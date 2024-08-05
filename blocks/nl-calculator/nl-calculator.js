a1ffa5cb8dcd1b8f36e621af2ea2f30a6b73c92c
ManjunathJ-MMSG <ManjunathJ@mmsg.com.au>
2024-08-05 21:18:38 +1000

    Update the nl-calculator block

--- blocks/nl-calculator/nl-calculator.js
+++ blocks/nl-calculator/nl-calculator.js
@@ -1,9 +1,3 @@
-import getMakes from './api/getMakes.js';
-import getModels from './api/getModels.js';
-import getAutoDetailOnMakeModel from './api/getAutoDetailOnMakeModel.js';
-import getVehicleImageOnNVIC from './api/getVehicleImageOnNVIC.js';
-import getAutoDetailOnNVIC from './api/getAutoDetailOnNVIC.js';
-
 export default async function decorate(block) {
   block.textContent = '';
   block.innerHTML = '';
