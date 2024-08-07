e6893e260d0e6f252cfeb58bbb35f4b93f03efdf
mojojain <manosezen@gmail.com>
2024-08-07 22:45:53 +0530

    added new url

--- blocks/form/request-callback.js
+++ blocks/form/request-callback.js
@@ -1,45 +1,45 @@
 import { CONTACTUS_SUCCESS_MESSAGE, errorResponseHandler } from './helper.js';
 
-function requestCallbackPayload(payload) {
-  // const transformedObject = {
-  //   Context: 'Contact Us',
-  //   From: payload.email,
-  //   FromName: `${payload.firstName} ${payload.lastName}`,
-  //   MessageHtml: `Name: ${payload.firstName}<br/>Surname: ${payload.lastName}<br/>Email: ${payload.email}<br/>Contact Number: ${payload.phone}<br/><br/>Message from Client: <br/><br/>${payload.enquiry}<br/><br/>`,
-  //   ReplyTo: payload.email,
-  //   Subject: `Contact us: ${payload.firstName} ${payload.lastName}`,
-  // };
-  const transformedObject = {
-    FirstName: payload.firstName,
-    LastName: 'MMSG fianl last',
-    Email: 'test@test.com',
-    BrandID: 'Remserv',
-    Channel: 'Digital',
-    SubChannel: 'Website',
-    Referrer: 'Generic',
-    ReferrerTeam: '',
-    EmployerCode: null,
-    MobilePhone: '0405599123',
-    OtherPhone: '0405599123',
-    EmployerName: 'Queensland Corrective Services',
-    State: 'Qld',
-    PreferredCallBackTime: '',
-    Notes: 'Testing please ignore',
-    Salary: '0.00',
-    OriginatingTeam: '',
-    VehicleType: '',
-    ConsultantTransferredTo: '',
-    SiteUrl:
-      'https://www.remservlease.com.au/vehicle-offers?cid=partner%3Aemail%3Afy24q4_vo_rs_qg_lg_%3Atgu_camp%3Afindmycar2&consultant=generic&utm_campaign=2024_All%20Member&utm_medium=email&_hsenc=p2ANqtz-_6ZwY5mTTcIFZPr3CIlmnm-NbEtr3vUV0L0CSVq_5Wd79kgzfwBx32RoNSODek9SZtSdcGiIroU3MORXWBuTJDbtiVWQ&_hsmi=308146742&utm_content=308146742&utm_source=hs_email',
-    ExistingEmployer: null,
-    UUID: '6dfe0695-4473-462d-ba64-48f2a8c7743d',
-    MarketingChannelSource: 'Email\r',
-    MarketingAttribution: 'cid=partner:email:fy24q4_vo_rs_qg_lg_:tgu_camp:findmycar2',
-    CampaignName: 'fy24q4_vo_rs_qg_lg_',
-  };
+// function requestCallbackPayload(payload) {
+//   // const transformedObject = {
+//   //   Context: 'Contact Us',
+//   //   From: payload.email,
+//   //   FromName: `${payload.firstName} ${payload.lastName}`,
+//   //   MessageHtml: `Name: ${payload.firstName}<br/>Surname: ${payload.lastName}<br/>Email: ${payload.email}<br/>Contact Number: ${payload.phone}<br/><br/>Message from Client: <br/><br/>${payload.enquiry}<br/><br/>`,
+//   //   ReplyTo: payload.email,
+//   //   Subject: `Contact us: ${payload.firstName} ${payload.lastName}`,
+//   // };
+//   const transformedObject = {
+//     FirstName: payload.firstName,
+//     LastName: 'MMSG fianl last',
+//     Email: 'test@test.com',
+//     BrandID: 'Remserv',
+//     Channel: 'Digital',
+//     SubChannel: 'Website',
+//     Referrer: 'Generic',
+//     ReferrerTeam: '',
+//     EmployerCode: null,
+//     MobilePhone: '0405599123',
+//     OtherPhone: '0405599123',
+//     EmployerName: 'Queensland Corrective Services',
+//     State: 'Qld',
+//     PreferredCallBackTime: '',
+//     Notes: 'Testing please ignore',
+//     Salary: '0.00',
+//     OriginatingTeam: '',
+//     VehicleType: '',
+//     ConsultantTransferredTo: '',
+//     SiteUrl:
+//       'https://www.remservlease.com.au/vehicle-offers?cid=partner%3Aemail%3Afy24q4_vo_rs_qg_lg_%3Atgu_camp%3Afindmycar2&consultant=generic&utm_campaign=2024_All%20Member&utm_medium=email&_hsenc=p2ANqtz-_6ZwY5mTTcIFZPr3CIlmnm-NbEtr3vUV0L0CSVq_5Wd79kgzfwBx32RoNSODek9SZtSdcGiIroU3MORXWBuTJDbtiVWQ&_hsmi=308146742&utm_content=308146742&utm_source=hs_email',
+//     ExistingEmployer: null,
+//     UUID: '6dfe0695-4473-462d-ba64-48f2a8c7743d',
+//     MarketingChannelSource: 'Email\r',
+//     MarketingAttribution: 'cid=partner:email:fy24q4_vo_rs_qg_lg_:tgu_camp:findmycar2',
+//     CampaignName: 'fy24q4_vo_rs_qg_lg_',
+//   };
 
-  return transformedObject;
-}
+//   return transformedObject;
+// }
 
 function requestCallbackResponse(form) {
   const divElement = document.createElement('div');
@@ -71,7 +71,7 @@ export default async function requestCallbackSubmission(form, payload) {
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify(requestCallbackPayload(payload)),
+    body: JSON.stringify(payload),
   };
   try {
     const response = await fetch(apiUrl, fetchOptions);
